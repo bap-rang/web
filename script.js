d5f3c6fb8e0c8713f2cd7ee23ee39a7d5b8d1401
Minh <minh17301@gmail.com>
2022-09-02 01:36:26 +0700

    add remove feature

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ const container = document.querySelector("#container")
 const numberArea = document.querySelector("#number_post")
 var number_post = 0
 var uploader = ""
- input.onclick = function(){
-     this.value = null;
+input.onclick = function(){
+    this.value = null;
 }
+
+function updateNumberPost()
+{
+    var str = number_post.toString().bold()
+        if (number_post > 1) { 
+            numberArea.innerHTML = str + " posts"
+        }
+        else {
+            numberArea.innerHTML = str + " post"
+        }
+}
+
 input.addEventListener("change", function(e) {
     const reader = new FileReader();
     reader.addEventListener("load", () =>{
@@ -15,13 +27,13 @@ input.addEventListener("change", function(e) {
         display.style.backgroundImage = `url(${uploader})`
         container.insertBefore(display, container.firstChild)
         number_post++
-        var str = number_post.toString().bold()
-        if (number_post > 1) { 
-            numberArea.innerHTML = str + " posts"
-        }
-        else {
-            numberArea.innerHTML = str + " post"
-        }
+        display.addEventListener("click", function(e) {
+            display.remove()
+            number_post--
+            updateNumberPost()
+        })
+        updateNumberPost()
     })
     reader.readAsDataURL(this.files[0])
 })
+
