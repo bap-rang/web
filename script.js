54c0149df54eefdc667adb60d224790ee1a9e850
Minh <minh17301@gmail.com>
2022-09-02 17:40:51 +0700

    blur background and pop-up image

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+const body = document.body;
 const input = document.querySelector("#image_input")
 const container = document.querySelector("#container")
 const numberArea = document.querySelector("#number_post")
+const popup = document.querySelector("#pop-up")
 var number_post = 0
 var uploader = ""
 input.onclick = function(){
@@ -18,19 +20,36 @@ function updateNumberPost()
     }
 }
 
+function toggle()
+{
+    var blur = document.querySelector("#blur")
+    blur.classList.toggle('active')
+}
+console.log(body)
+// body.addEventListener("click", function(e) {
+//     if (e.target.classList === "")
+//     //toggle()
+// })
 input.addEventListener("change", function(e) {
     const reader = new FileReader();
     reader.addEventListener("load", () =>{
         uploader = reader.result
         var display = document.createElement("div")
         display.setAttribute("id", "display_image")
+        display.setAttribute("src", `url(${uploader})`)
         display.style.backgroundImage = `url(${uploader})`
         container.insertBefore(display, container.firstChild)
         number_post++
         display.addEventListener("click", function(e) {
-            display.remove()
-            number_post--
-            updateNumberPost()
+            //display.remove()
+            //number_post--
+            //updateNumberPost()
+            toggle()
+            var popupimg = document.createElement("div")
+            popupimg.setAttribute("id", "display_image")
+            popupimg.style.backgroundImage = display.getAttribute("src")
+            popupimg.style.transform = "scale(2)";
+            popup.insertBefore(popupimg, popup.firstChild)
         })
         updateNumberPost()
     })
